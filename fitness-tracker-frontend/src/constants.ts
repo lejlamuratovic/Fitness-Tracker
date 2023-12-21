188ac4ad3de13720d8a294be78f8f05feee0edf2
lejlamrt <lejlamuratovic6@gmail.com>
2023-12-21 17:56:18 +0100

    exercise card and exercise list added

---
+++ fitness-tracker-frontend/src/constants.ts
@@ -0,0 +1,46 @@
+import { Exercise } from "./utils/types";
+
+export const exerciseList: Exercise[] = [
+    {
+        id: "657f57eae9630033a054dd5b",
+        name: "Lat Pulldown",
+        muscleGroup: "LATS",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
+        imageUrl: "https://s3.eu-central-1.amazonaws.com/fitness-tracker-bucket/1703175329577-062f66e4986e4035892db9ae59999df2IfulbDgKAtYMjRG5-0.png"
+    },
+    {
+        id: "657f57eae9630033a054dd5b",
+        name: "Bicep Curl",
+        muscleGroup: "BICEPS",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
+        imageUrl: "https://s3.eu-central-1.amazonaws.com/fitness-tracker-bucket/1703175329577-062f66e4986e4035892db9ae59999df2IfulbDgKAtYMjRG5-0.png"
+    }, 
+    {
+        id: "657f57eae9630033a0dddd5b",
+        name: "Lat Pulldown",
+        muscleGroup: "LATS",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
+        imageUrl: "https://s3.eu-central-1.amazonaws.com/fitness-tracker-bucket/1703175329577-062f66e4986e4035892db9ae59999df2IfulbDgKAtYMjRG5-0.png"
+    },
+    {
+        id: "657f579630000033a054dd5b",
+        name: "Bicep Curl",
+        muscleGroup: "BICEPS",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
+        imageUrl: "https://s3.eu-central-1.amazonaws.com/fitness-tracker-bucket/1703175329577-062f66e4986e4035892db9ae59999df2IfulbDgKAtYMjRG5-0.png"
+    }, 
+    {
+        id: "65777eae9630033a0dddd5b",
+        name: "Lat Pulldown",
+        muscleGroup: "LATS",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
+        imageUrl: "https://s3.eu-central-1.amazonaws.com/fitness-tracker-bucket/1703175329577-062f66e4986e4035892db9ae59999df2IfulbDgKAtYMjRG5-0.png"
+    },
+    {
+        id: "6969579630000033a054dd5b",
+        name: "Bicep Curl",
+        muscleGroup: "BICEPS",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
+        imageUrl: "https://s3.eu-central-1.amazonaws.com/fitness-tracker-bucket/1703175329577-062f66e4986e4035892db9ae59999df2IfulbDgKAtYMjRG5-0.png"
+    }
+]
---
+++ fitness-tracker-frontend/src/utils/types.ts
@@ -0,0 +1,7 @@
+export type Exercise = {
+    id: String, 
+    name: String,
+    muscleGroup: String,
+    description: String,
+    imageUrl: String
+}
