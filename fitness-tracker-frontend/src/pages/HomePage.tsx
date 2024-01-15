4e14a7079d5903d92932869e1d4bf967af2c69fe
lejlamrt <lejlamuratovic6@gmail.com>
2024-01-15 21:16:34 +0100

    new homepage

--- fitness-tracker-frontend/src/pages/HomePage.tsx
+++ fitness-tracker-frontend/src/pages/HomePage.tsx
@@ -1,8 +1,65 @@
+import { Button, Container, Typography } from '@mui/material'
+import Background from '../assets/background.jpeg'
+import { Link } from 'react-router-dom'
+
+
+const backgroundStyle = {
+  backgroundImage: `url(${Background})`,
+  backgroundSize: 'cover',
+  backgroundPosition: 'center',
+  backgroundRepeat: 'no-repeat',
+  height: '100vh',
+  width: '100vw',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  flexDirection: 'column'
+}
+
 const HomePage = () => {
   return (
-    <div>
-      HOMEPAGE
-    </div>
+    <Container maxWidth="xl" sx={ backgroundStyle }>
+      <Typography 
+        variant="h2" 
+        color="white" 
+        sx={{ 
+          textTransform: 'uppercase', 
+          fontWeight: 'bold', 
+          letterSpacing: '5px', 
+          textShadow: '2px 2px 2px black'}}>
+            Fitness Tracker
+      </Typography>
+      <Typography 
+        variant="h6" 
+        color="white" 
+        sx={{ 
+          textTransform: 'uppercase', 
+          fontWeight: 'bold', 
+          letterSpacing: '5px', 
+          textShadow: '1px 1px 1px black'}}>
+            Track your fitness goals
+      </Typography>
+      <Button 
+        variant="contained" 
+        color="primary" 
+        size="large" 
+        sx={{ 
+          width: '300px', 
+          height: '50px', 
+          backgroundColor: '#72A1BF', 
+          marginTop: '20px',
+          fontSize: '20px',
+          fontWeight: 'bold',
+          '&:hover': {
+            color: 'white'
+          } 
+        }}
+        component={ Link }
+        to="/register"
+        >
+          Get Started
+        </Button>
+    </Container>
   )
 }
 
