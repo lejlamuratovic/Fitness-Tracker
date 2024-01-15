012baf583019115e08e7771c0d518af69f93e8c0
lejlamrt <lejlamuratovic6@gmail.com>
2024-01-15 19:35:54 +0100

    not found page

--- fitness-tracker-frontend/src/pages/NotFoundPage.tsx
+++ fitness-tracker-frontend/src/pages/NotFoundPage.tsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import { Button, Typography } from '@mui/material'
+import { Link } from 'react-router-dom'
+import NotFound from '../assets/404.png'
 
 const NotFoundPage = () => {
   return (
-    <div>
-      Not Found
-    </div>
+    <>
+      <Typography variant="h3" color="text.secondary">404 Not Found</Typography>
+      <img src={ NotFound } alt="404 Not Found" style={{ width: '300px' }}/>
+      <Typography variant="h5" color="text.secondary">The page you are looking for does not exist.</Typography>
+      <Button 
+        variant="contained" 
+        color="primary" 
+        size="large" 
+        sx={{ 
+          width: '300px', 
+          height: '50px', 
+          backgroundColor: '#72A1BF', 
+          marginTop: '20px',
+          '&:hover': {
+            color: 'white'
+          } 
+        }}
+        component={ Link }
+        to="/"
+        >
+          Go Back to Homepage
+        </Button>
+    </>
   )
 }
 
